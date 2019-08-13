f672792c1413f77da5d2e9ca3efba1f8ad72d4c9
Felipe Herranz <felhr85@gmail.com>
2019-08-13 02:12:00 +0200

    fixed update win script

--- scripts/win_setup.sh
+++ scripts/win_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-ADB_PATH= $1                            
+if [[ -z $1 ]]; then
+    echo "path argument is needed"
+    exit 1
+fi
 
 WINDOWS_USER=$(whoami.exe | awk -F "\\" '{print $2}')
 MOUNT_C=$(cat /proc/mounts | grep C: | awk '{print $2}')
 HOME_FOLDER=$MOUNT_C/Users/${WINDOWS_USER/$'\r'/}
-ex -sc "28d|x" $HOME_FOLDER/.air-adb/air-adb.sh && ex -sc "28i|echo $ADB_PATH" -cx $HOME_FOLDER/.air-adb/air-adb.sh
+ex -sc "28d|x" $HOME_FOLDER/.air-adb/air-adb.sh && ex -sc "28i|                echo $1" -cx $HOME_FOLDER/.air-adb/air-adb.sh
